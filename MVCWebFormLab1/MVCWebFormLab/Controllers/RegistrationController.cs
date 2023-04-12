e62ee0e852a5e14c35839076337a861d01ee4f48
Asma Razaq <asmarazaq@rocketmortgage.com>
2023-04-12 08:57:50 -0400

    Added new lab for Coffee shop

---
+++ MVCWebFormLab1/MVCWebFormLab/Controllers/RegistrationController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCWebFormLab.Models;
+
+namespace MVCWebFormLab.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Welcome", userModel);
+        }
+
+        //public IActionResult Welcome(UserModel userModel)
+        //{
+        //    //ViewBag.FirstName = firstName;
+        //    return View(userModel);
+        //}
+
+        public IActionResult Welcome()
+        {
+            List<string> menuItems = new List<string>()
+            {
+                "Latte, $2.0",
+                "Capuccino, $3.5",
+                "Muffin, $4.6",
+                "Croissant, $2.15",
+                "Salad, $7.15",
+                "Fruit Cup, $1.3",
+                "chai, $2.2",
+                "bagal, $3.1",
+                "pastry, $4.2"
+
+            };
+
+            return View(menuItems);
+        }
+        public IActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCWebFormLab1/MVCWebFormLab/Models/UserModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCWebFormLab.Models
+{
+    public class UserModel
+    {        
+        [Required]
+        [Display(Name = "FirstName")]
+        [StringLength(20, ErrorMessage = "First name length should not exceed")]
+        public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last Name is required")]
+        [Display(Name = "LastName")]
+        [StringLength(20, ErrorMessage = "Last name length should not exceed")]
+        public string LastName { get; set; }
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", 
+            ErrorMessage = "plz enter valid email address")]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$",
+            ErrorMessage = "Password must be 8-15 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.")]
+        public string Password { get; set; }
+    }
+}
